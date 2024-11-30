187c5f880a8bd6370fd5dfc2288c1892cbc44962
manunin <manunin@outlook.com>
2024-12-01 00:22:15 +0100

    G1: integration with OpenAi is added

--- src/main/java/com/manunin/grapher/domain/fileparser/service/FileParserServiceImpl.java
+++ src/main/java/com/manunin/grapher/domain/fileparser/service/FileParserServiceImpl.java
@@ -2,6 +2,8 @@ package com.manunin.grapher.domain.fileparser.service;
 
 import com.manunin.grapher.core.Node;
 import com.manunin.grapher.domain.fileparser.dto.Link;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -15,9 +17,30 @@ import java.util.stream.Stream;
 
 @Component
 public class FileParserServiceImpl implements FileParserService {
+
+    private final ChatModel aiClient;
+
+    public FileParserServiceImpl(@Qualifier("openAiChatModel") ChatModel chatModel) {
+        this.aiClient = chatModel;
+    }
+
     @Override
     public List<Link> parseFile(InputStream file) {
-        return processFileLines(file, FileParserServiceImpl::parse);
+        String prompt = """
+                Find all relationships of the type "%s" (the type means semantic meaning) in the text: "%s". Return result as a relationships of links in format:
+               <id>,<source>,<target>,<type>. Each line in the text is a separate relationship.
+               For example, if the text is "John resides in New York. Mary lives in Los Angeles." and the type is "lives in", the result should be:
+                1,John,New York,lives in
+                2,Mary,Los Angeles,lives in
+               """;
+
+        String text = processFileLines(file, FileParserServiceImpl::linesToText);
+        String aiResponse = aiClient.call(String.format(prompt, "lives in", text));
+        return parse(aiResponse.lines());
+    }
+
+    private static String linesToText(Stream<String> lines) {
+        return lines.collect(Collectors.joining());
     }
 
     private static <T> T processFileLines(final InputStream file, final Function<Stream<String>, T> func) {
